2e0317e5cb33e205e4d6859571dfa7637c65a1e7
nubrixsecurity <142175078+nubrixsecurity@users.noreply.github.com>
2024-06-11 14:47:09 -0500

    Add files via upload

---
+++ M365SAT/M365SATTester.ps1
@@ -0,0 +1,41 @@
+#Requires -Version 5.1
+#Requires -RunAsAdministrator
+
+param ($outPath,$userPrincipalName,$services)
+
+function ExecuteM365SAT
+{
+	Import-Module .\M365SAT.psd1
+
+	Get-M365SATReport `
+		-OutPath $outPath `
+		-Username $userPrincipalName `
+		-reportType "HTML" `
+		-AllowLogging "Warning" `
+		-Modules All `
+		-BenchmarkVersion LATEST `
+		-LicenseMode All `
+		-LicenseLevel All `
+		-EnvironmentType $services
+  
+	Remove-Module M365SAT -Force
+}
+
+
+function CheckAdminPrivBeta
+{
+	# Check if script is running as Adminstrator and if not use RunAs
+	Write-Host "[...] Checking if the script is running as Administrator"
+	$IsAdmin = ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+	if (-not $IsAdmin)
+	{
+		Write-Warning "[!] Program needs Administrator Rights! Trying to Elevate to Admin..."
+		Start-Process powershell -Verb runas -ArgumentList "-NoExit -c cd '$pwd'; .\M365SATTester.ps1"
+	}
+	else
+	{
+		Write-Host "[+] The script is running as Administrator..." -ForegroundColor Green
+		ExecuteM365SAT
+	}
+}
+CheckAdminPrivBeta
